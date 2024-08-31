d34be853c3048385373d590b2849d8aa2587a266
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-08-31 09:52:18 +0000

    trabajando en diseño

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,34 +1,43 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
     const { store, actions } = useContext(Context);
 
-    // Llama a getUsers cuando el componente se monta
+    // Llama a getUsers, getPost y getPostsUser cuando el componente se monta
     useEffect(() => {
         actions.getUsers(); 
         actions.getPost(); // Asegúrate de que esta función está implementada en tu store
         actions.getPostsUser(3);
     }, []);  // [] asegura que solo se llama una vez al montar el componente
- console.log(store.users)
- console.log(store)
+
+    console.log(store.users);
+    console.log(store);
+
     return (
-        <div className="text-center mt-5">
-            <h1>Post User feel !!</h1>
+        <div className="container text-center mt-5">
+            <h1>Post User Feel !!</h1>
+            
+            {/* Tarjeta */}
+            <div className="card mx-auto" style={{ width: '18rem' }}>
+                <div className="card-body">
+                    <h5 className="card-title">Card Title</h5>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">Card Subtitle</h6>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="card-link">Card Link</a>
+                    <a href="#" className="card-link">Another Link</a>
+                </div>
+            </div>
         
-            {/* Renderiza la lista de usuarios */}
-			<div className="container">
-			<div className="row">
-			<div className="col list">
+            {/* Lista de usuarios */}
             <div className="mt-4">
                 <h2>Users List</h2>
                 {store.users.length > 0 ? (
-                    <ul className="list-group">
+                    <ul className="list-group mx-auto" style={{ maxWidth: '600px' }}>
                         {store.users.map(user => (
                             <li key={user.id} className="list-group-item">
-                               {user.user_name}-{user.id}
+                                {user.user_name} - {user.id}
                             </li>
                         ))}
                     </ul>
@@ -36,24 +45,31 @@ export const Home = () => {
                     <p>No users available</p>
                 )}
             </div>
-						</div>
-						<div className="col post mt-4">
-                        <h2>Publicaciones</h2>
-                        {store.post.length > 0 ? (
-                    <ul className="list-group">
+            
+            {/* Publicaciones */}
+            <div className="mt-4">
+                <h2>Publicaciones</h2>
+                {store.post.length > 0 ? (
+                    <ul className="list-group mx-auto bg-gradient" style={{ maxWidth: '600px' }}>
                         {store.post.map(post => (
                             <li key={post.id} className="list-group-item">
-                             {post.title}...  {post.content} - {post.user_id}
+                                - - 
+                                <div className="card mx-auto" style={{ width: '18rem' }}>
+                <div className="card-body">
+                    <h5 className="card-title">{post.title} </h5>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">{post.user_id}</h6>
+                    <p className="card-text"> {post.content}</p>
+                    <a href="#" className="card-link">Card Link</a>
+                    <a href="#" className="card-link">Another Link</a>
+                </div>
+            </div>
                             </li>
                         ))}
                     </ul>
                 ) : (
-                    <p>No users available</p>
+                    <p>No posts available</p>
                 )}
-						</div>
-						
-				</div>
-			</div>
+            </div>
         </div>
     );
 };
